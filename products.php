2594da5b7402a1d2df01d3901dd92251c239bc79
NicolaDiQuinzio <118384442+NicolaDiQuinzio@users.noreply.github.com>
2023-05-22 00:33:45 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,26 +1,7 @@
 <?php
-require_once 'classes.php';
+$category1 = new Category(1, 'Dogs', 'fa-dog');
+$category2 = new Category(2, 'Cats', 'fa-cat');
 
-// Creo delle categorie
-$category1 = new Category(1, "Cani", "fa-dog");
-$category2 = new Category(2, "Gatti", "fa-cat");
-
-// Crep degli oggetti Product
-$product1 = new Product(1, "Cibo per cani", 10.99, "", $category1, "Cibo");
-$product2 = new Product(2, "Gioco per gatti", 5.99, "", $category2, "Gioco");
-
-// Creo dello Shop
-$shop = new Shop();
-$shop->addProduct($product1);
-$shop->addProduct($product2);
-$shop->addCategory($category1);
-$shop->addCategory($category2);
-
-// Genero le cards dei prodotti
-$productCards = '';
-foreach ($shop->products as $product) {
-    $productCards .= $product->generateCard();
-}
-
-echo $productCards;
-?>
+$product1 = new Product(1, 'Dog Food', 10.99, $category1, 'Food');
+$product2 = new Product(2, 'Cat Toy', 5.99, $category2, 'Toy');
+$product3 = new Product(3, 'Dog Bed', 29.99, $category1, 'Bed');
